5b539178c3359f58395680f00d2ad110f40ae4fc
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-12-21 12:03:40 +0530

    !status

--- cool.js
+++ cool.js
@@ -20,30 +20,30 @@ client.on('message', message => {
 		message.reply(enhanceChat.embedStatic(helpText));
 
 	} else if (message.content === '!hello') {
-		
+
 		message.reply(enhanceChat.embedStatic('world! ha ha gotcha 😎'));
-	
+
 	} else if (message.content === '!yo') {
-	
+
 		message.reply('lo! 😜');
-	
+
 	} else if (
-	
+
 		message.content === 'what is my avatar' ||
 		message.content === 'what\'s my avatar' ||
 		message.content === 'how do i look') {
     message.reply(enhanceChat.embedStatic('Your avatar', 'Great! 👌 ', '#00b8d4', message.author.avatarURL));
-	
+
 	} else if (message.content.startsWith('!cool')) {
-	
+
 		const args = (message.content.split('!cool').pop()).trim();
 		if(args.length >= 1) {
-			
+
 			axios.get(`https://cool-name-api.glitch.me/coolify?name=${args}/`).then(response => {
 				const coolNames = enhanceChat.jsonToTable(response.data);
-				message.reply(enhanceChat.embedStatic(coolNames, '', '#00b8d4'));	
+				message.reply(enhanceChat.embedStatic(coolNames, '', '#00b8d4'));
 			}).catch(err => console.log(err.response));
-		
+
 		} else {
 			message.reply(enhanceChat.embedStatic(
 				'Expected at least 1 param after !cool. i.e   **!cool text text text....**',
@@ -53,23 +53,38 @@ client.on('message', message => {
         'https://git.io/fpFgn'
       ));
 		}
-	
+
 	} else if (message.content.startsWith('!uncool')) {
-	
+
 		const args = (message.content.split('!uncool').pop()).trim();
 		if(args.length >= 1) {
 
 			axios.get(`https://cool-name-api.glitch.me/uncoolify?name=${args}/`).then(response => {
 				// TODO: That's a jugad, find out why there's forward '/' in response
 				const uncoolNames = (response.data['uncool_name'][0]).slice(0, -1);
-				console.log(response.data['uncool_name'][0]);
-				message.reply(enhanceChat.embedStatic(uncoolNames, '', '#00b8d4'));	
+				// console.log(response.data['uncool_name'][0]);
+				message.reply(enhanceChat.embedStatic(uncoolNames, '', '#00b8d4'));
 			}).catch(err => console.log(err.response));
-		
+
 		} else {
 			message.reply('Expected at least 1 param after !uncool. i.e   **!uncool t3xt t3xt text....**');
 		}
-	
+
+	} else if (message.content.startsWith('!status')) {
+		axios.get('https://cool-name-api.glitch.me/coolify?name=only4/').then(response => {
+			message.reply(enhanceChat.embedStatic(
+				'cool-name-api working status 200 ✅',
+				'Status 200',
+				'#00ec3c',
+			));
+		}).catch(err => {
+			console.log(err.response)
+			message.reply(enhanceChat.embedStatic(
+				'cool-name-api not working, ❌',
+				'Status ' + err.response.status,
+				'#bf0000',
+			));
+		});
 	}
 });
 
