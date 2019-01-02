47e8de9278314200f78371ae3164fed5e94e8e2c
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2019-01-02 07:45:40 +0530

    help in bots channel

--- cool.js
+++ cool.js
@@ -20,7 +20,7 @@ client.login(process.env.TOKEN);
 
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find(ch => ch.name === 'general');
+  const channel = member.guild.channels.find(ch => ch.name === 'bots');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
